4f7afc9d88bfa9dfaa72e7120d013fceb4fbff11
emredogan-cloud <ed897854@gmail.com>
2026-02-17 16:01:06 +0300

    fix: backend.tf key path

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "cloud-janitor82ee1d0c"
-    key = "cloud-janitor-ebs-tfstate-file"
+    key = "cloud-janitor-ebs-tfstate-file/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "cloud-janitor-state-table"
     encrypt = true
