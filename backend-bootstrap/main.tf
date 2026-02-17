15601819d15d7642d81c16b09627aa7bee30c507
emredogan-cloud <ed897854@gmail.com>
2026-02-17 16:05:49 +0300

    style: terraform fmt fix

--- backend-bootstrap/main.tf
+++ backend-bootstrap/main.tf
@@ -14,9 +14,9 @@ resource "aws_s3_bucket" "state_bucket" {
 resource "aws_s3_bucket_public_access_block" "name" {
   bucket = aws_s3_bucket.state_bucket.id
 
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
@@ -39,7 +39,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
 }
 
 resource "aws_dynamodb_table" "state_table" {
-  name = "cloud-janitor-state-table"
+  name         = "cloud-janitor-state-table"
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "LockID"
--- backend-bootstrap/versions.tf
+++ backend-bootstrap/versions.tf
@@ -3,13 +3,13 @@ terraform {
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
 
     random = {
-        source = "hashicorp/random"
-        version = "~> 3.6"
+      source  = "hashicorp/random"
+      version = "~> 3.6"
     }
   }
 }
